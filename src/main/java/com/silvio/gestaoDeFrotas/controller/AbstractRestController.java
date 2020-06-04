ed83b639ffa777e2953de847ad39a9348198244c
silvioantonio <oliveira_0000@hotmail.com>
2020-06-04 11:50:27 -0300

    Atualiza post

--- src/main/java/com/silvio/gestaoDeFrotas/controller/AbstractRestController.java
+++ src/main/java/com/silvio/gestaoDeFrotas/controller/AbstractRestController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -48,7 +49,7 @@ public abstract class AbstractRestController<ID, T> {
 		return t.get();		
 	}
 	
-	@RequestMapping(method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<T> salvar (@Valid @RequestBody T t, HttpServletResponse response)  {
 		T t2 = this.getService().getRepository().save(t);
 		
