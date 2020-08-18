45e261ae507c406f0ab5a47481c70b0cf3e88202
silvioantonio <oliveira_0000@hotmail.com>
2020-08-18 08:51:16 -0300

    atualiza endpoint

--- src/main/java/com/silvio/gestaoDeFrotas/controller/OrdemTrafegoController.java
+++ src/main/java/com/silvio/gestaoDeFrotas/controller/OrdemTrafegoController.java
@@ -2,14 +2,25 @@ package com.silvio.gestaoDeFrotas.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.silvio.gestaoDeFrotas.model.Condutor;
 import com.silvio.gestaoDeFrotas.model.OrdemDeTrafico;
+import com.silvio.gestaoDeFrotas.model.Veiculo;
+import com.silvio.gestaoDeFrotas.repository.CondutorRepository;
+import com.silvio.gestaoDeFrotas.repository.VeiculoRepository;
 import com.silvio.gestaoDeFrotas.service.AbstractRestService;
 import com.silvio.gestaoDeFrotas.service.OrdemTrafegoService;
 
@@ -23,6 +34,12 @@ public class OrdemTrafegoController extends AbstractRestController<Long,OrdemDeT
 
 	@Autowired
 	private OrdemTrafegoService ordemTrafegoService;
+	
+	@Autowired
+	private CondutorRepository condutorRepository;
+	
+	@Autowired
+	private VeiculoRepository veiculoRepository;
 	
 	@GetMapping("/veiculo/{id}")
 	public List<OrdemDeTrafico> buscarTodosPorVeiculo(@PathVariable Long id){
@@ -48,6 +65,18 @@ public class OrdemTrafegoController extends AbstractRestController<Long,OrdemDeT
 	public List<OrdemDeTrafico> buscaPorDestino(@PathVariable String destino){
 		return this.ordemTrafegoService.buscaOndensPorDestino(destino);
 	}
+	
+	@PostMapping
+	public ResponseEntity<OrdemDeTrafico> salvar (@Valid @RequestBody OrdemDeTrafico ordem, HttpServletResponse response)  {
+		  Condutor c = (Condutor) condutorRepository.findByMatricula(ordem.getCondutor().getMatricula());
+		  ordem.setCondutor(c);
+		  
+		  Veiculo v = (Veiculo) veiculoRepository.findByPlaca(ordem.getVeiculo().getPlaca());
+		  ordem.setVeiculo(v);
+
+		  OrdemDeTrafico t2 = this.getService().getRepository().save(ordem);
+		  return ResponseEntity.status(HttpStatus.CREATED).body(t2);
+		}
 	
 	@Override
 	public AbstractRestService<OrdemDeTrafico, Long> getService() {
